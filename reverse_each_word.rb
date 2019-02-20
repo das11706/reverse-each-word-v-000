6decffa7d6d51d09db989dc91e5d81ec36a0d184
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-20 02:11:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,8 +7,8 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
-  sentence.split.join(" ")
-  sentence.collect do |word|
+  sentence
+  #sentence.collect do |word|
 
-  end
+  #end
 end
