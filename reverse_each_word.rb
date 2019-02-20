223ea5a17d767b2771b597f530190da8c0e37007
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-20 02:22:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,7 @@
 def reverse_each_word(sentence)
   rev_sentence = []
   sentence.split.each do |word|
-  rev_sentence << word.reverse!
+  rev_sentence << word.reverse
   end
   rev_sentence.join(" ")
-end
-
-def reverse_each_word(sentence)
-  #rev_sentence = []
-  sentence.split.collect do |word|
-  word.reverse!
-  end
-  sentence.join(" ")
 end
